c2e68ae295ff63867638c22a935892dbed31ed80
zabeer001 <binzabirtareq@gmail.com>
2025-07-01 16:14:40 +0600

    udpated controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,12 @@
 import { getAllUsers } from '../services/users/user.service.js';
 
 export const index = async (req, res) => {
-  try {
-    const users = await getAllUsers();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+
+    try {
+        // const age = req.query.age; get something from param
+        const users = await getAllUsers();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
